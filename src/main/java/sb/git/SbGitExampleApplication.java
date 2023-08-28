d800219bb0e8bbe4e08805f944951267a1c1977a
pragsgen31 <pragyaswamigenpact@gmail.com>
2023-08-28 16:17:07 +0530

    Initial Commit

---
+++ src/main/java/sb/git/SbGitExampleApplication.java
@@ -0,0 +1,13 @@
+package sb.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbGitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbGitExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sb/git/SbGitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbGitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
